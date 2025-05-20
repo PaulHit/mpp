1a38cd79ddabb2a639302c74219abb54fabd9c36
PaulHit <hiticaspaul@gmail.com>
2025-05-20 23:01:02 +0300

    jmeter

--- movie-management-app/project/src/lib/supabase.ts
+++ movie-management-app/project/src/lib/supabase.ts
@@ -7,11 +7,6 @@ dotenv.config();
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log("Environment variables in supabase.ts:", {
-	url: supabaseUrl,
-	key: supabaseAnonKey ? "present" : "missing",
-});
-
 if (!supabaseUrl) throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 if (!supabaseAnonKey)
 	throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
